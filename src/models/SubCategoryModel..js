72d2968b3685b20e7c26034301a72faeb1722e8b
tanujkumar11 <tanujkumar11>
2024-06-29 00:36:25 +0530

    changes for subcategory module

--- src/models/SubCategoryModel..js
+++ src/models/SubCategoryModel..js
@@ -6,31 +6,69 @@ import { DataMapper } from "@aws/dynamodb-data-mapper";
 import { v4 as uuidv4 } from "uuid";
 
 // Update AWS configuration
-const dynamoDBClient = new DynamoDB({ region: process.env.Aws_region });
-dynamoose.aws.sdk = dynamoDBClient;
+// const dynamoDBClient = new DynamoDB({ region: process.env.Aws_region });
+// dynamoose.aws.sdk = dynamoDBClient;
 
-const schema = new dynamoose.Schema(
+// const schema = new dynamoose.Schema(
+//   {
+//     id: {
+//       type: String,
+//       required: false,
+//     },
+//     category_id: { type: String, required: true },
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     status: {
+//       type: Boolean,
+//       required: false,
+//       default: true
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// const SubCategoryModel = dynamoose.model("sub_category", schema
+// );
+// export default SubCategoryModel;
+
+import { DataTypes } from "sequelize";
+import dbConnection from "../config/dbConfig.js";
+
+const SubCategoryModel = dbConnection.define(
+  "subcategory",
   {
     id: {
-      type: String,
-      required: false,
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
     },
-    category_id: { type: String, required: true },
-    name: {
-      type: String,
-      required: true,
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
     status: {
-      type: Boolean,
-      required: false,
-      default: true
+      type: DataTypes.ENUM("active", "inactive"),
+      defaultValue: "active"
     },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: false, tableName: "subcategory" }
 );
 
-const SubCategoryModel = dynamoose.model("sub_category", schema
-);
 export default SubCategoryModel;
